734ea78a57b86f987038c1274a9ddf413e6d2cfb
jerrysheen <jerryshin961115@gmail.com>
2024-03-10 18:23:35 +0800

    D：调整目录结构

--- Chapter4_Init_Direct3D/InitDirect3DApp.cpp
+++ Chapter4_Init_Direct3D/InitDirect3DApp.cpp
@@ -6,48 +6,7 @@
 //
 //***************************************************************************************
 
-#include "../../Common/d3dApp.h"
-#include <DirectXColors.h>
-
-using namespace DirectX;
-
-class InitDirect3DApp : public D3DApp
-{
-public:
-	InitDirect3DApp(HINSTANCE hInstance);
-	~InitDirect3DApp();
-
-	virtual bool Initialize()override;
-
-private:
-    virtual void OnResize()override;
-    virtual void Update(const GameTimer& gt)override;
-    virtual void Draw(const GameTimer& gt)override;
-
-};
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
-				   PSTR cmdLine, int showCmd)
-{
-	// Enable run-time memory check for debug builds.
-#if defined(DEBUG) | defined(_DEBUG)
-	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
-#endif
-
-    try
-    {
-        InitDirect3DApp theApp(hInstance);
-        if(!theApp.Initialize())
-            return 0;
-
-        return theApp.Run();
-    }
-    catch(DxException& e)
-    {
-        MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
-        return 0;
-    }
-}
+#include "InitDirect3DApp.h"
 
 InitDirect3DApp::InitDirect3DApp(HINSTANCE hInstance)
 : D3DApp(hInstance) 
